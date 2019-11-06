c0a9771f315a6c6557beadb5e40b19f702dd50b7
pedrivas <drivaspedro@gmail.com>
2019-11-06 17:18:40 -0300

    Adicionado menu superior horizontal

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+var dados = document.getElementById("dados");
+var HTML = dados.innerHTML;
 
+function teste() {
+    //var contHome = document.getElementById("pills-home");
+    //contHome.innerHTML = 'tESTE COM JAVASCRIPT';
+    dados.innerHTML = `teste dados`;
+
+}
